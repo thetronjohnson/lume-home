4515e39931eb42db0a1810e703d6f5e1199a807f
Kiran Johns <kiranjohns369@gmail.com>
2023-09-21 22:37:41 +0530

    hero section

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <Navbar/>
+        <div id="hero" class="place-content-center content-center text-center py-5">
+            <p class="text-7xl font-extrabold">Lume</p>
+            <p class="text-3xl font-semibold py-5">A beautiful & powerful nostr client for desktop</p>
+
+            <button class="btn btn-primary">Download Now!</button>
+            <p class="text-lg py-3">For GNU/Linux, macOS & Windows</p>
+
+            <div class="flex justify-center items-center">
+            <img class="object-scale-down w-10/12" src="~/assets/images/lume-1.png" alt="Lume Desktop App"/>
+            </div>
+        </div>
+    </div>
+</template>
